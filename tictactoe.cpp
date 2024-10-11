cebb0b04105c2c2331b8a5cc1ae5d2df3f1d6752
TRIUYEN_TANG <triuyentang@gmail.com>
2024-10-11 17:49:45 +0200

    [WIP] Working protorype

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -51,14 +51,15 @@ int main()
 
 void drawBoard(char *spaces){
     std::cout << "\n";
-    std::cout << "   |    |   " << '\n';
-    std::cout << "   " << spaces[0] << "  |  " << spaces[1] <<"  |  "<< spaces[2] << '\n';
-    std::cout << "____|____|____" << '\n';
-    std::cout << "    |    |    " << '\n';
-    std::cout << "   " << spaces[3] << "  |  " << spaces[4] <<"  |  "<< spaces[5] <<'\n';
-    std::cout << "____|____|____" << '\n';
-    std::cout << "    |    |    " << '\n';
-    std::cout << "   " << spaces[6] << "  |  " << spaces[7] <<"  |  "<< spaces[8] <<'\n';
+    std::cout << "     |     |   " << '\n';
+    std::cout << "  " << spaces[0] << "  |  " << spaces[1] <<"  |  "<< spaces[2] << '\n';
+    std::cout << " ____|_____|____" << '\n';
+    std::cout << "     |     |    " << '\n';
+    std::cout << "  " << spaces[3] << "  |  " << spaces[4] <<"  |  "<< spaces[5] <<'\n';
+    std::cout << " ____|_____|____" << '\n';
+    std::cout << "     |     |    " << '\n';
+    std::cout << "  " << spaces[6] << "  |  " << spaces[7] <<"  |  "<< spaces[8] <<'\n';
+    std::cout << "     |     |   " << '\n';
     std::cout << "\n";
 }
 void playerMove(char *spaces, char player){
